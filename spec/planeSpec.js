5632dd4432d4a690d69bfdd890f2c682ac2e01b4
hasulica <raduasandei@gmail.com>
2015-11-11 14:36:50 +0000

    Changed tests

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -5,8 +5,12 @@ describe('plane', function() {
      plane = new Plane();
   });
 
-  it('plane can land', function() {
-    expect(plane.land).toBeDefined();
+  it('plane can takeOff', function() {
+    expect(plane.takeOff).toBeDefined();
+  });
+
+  it('raises an error if already flying', function() {
+    expect(plane.takeOff).toThrowError("Plane cannot take off, already flying!");
   });
 
 });
